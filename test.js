9d39fe5f176962159b987ca2e4addd0ff973fdc5
Robin Millette <robin@millette.info>
2017-06-16 16:30:19 -0400

    More tests.

--- test.js
+++ test.js
@@ -2,13 +2,29 @@
 import test from 'ava'
 import { DatLru } from './'
 
-test('acc', async t => {
+const nop = () => Promise.resolve()
+
+test('init', async t => {
   const lru = new DatLru('/tmp/false95959')
-  const store = await lru.isReady()
-  t.truthy(store)
+  const ready = await lru.isReady()
+  t.truthy(ready)
 })
 
-test('bad', async t => {
+test('not ready', async t => {
   const lru = new DatLru('/tmp/false95959', { mkdirStrict: true })
   await t.throws(lru.isReady())
+})
+
+test('bad key', async t => {
+  const lru = new DatLru('/tmp/false95959', { mkdirError: false })
+  const fn = lru.get.bind(lru, 'abc1293')
+  const dat = lru.isReady().then(nop).then(fn)
+  await t.throws(dat)
+})
+
+test('init dat', async t => {
+  const lru = new DatLru('/tmp/false95959', { mkdirError: false })
+  const fn = lru.get.bind(lru, '49bd045de3beb9abcb7272967e2fb16e07b96c06e15cd814f703e8581d4561e5')
+  const dat = await lru.isReady().then(nop).then(fn)
+  t.truthy(dat && dat.archive)
 })
