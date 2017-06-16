40816405a502b7ec0d2eeffdb266f97473a3fb6c
Robin Millette <robin@millette.info>
2017-06-16 13:14:17 -0400

    Use dat-lru module.

--- cli.js
+++ cli.js
@@ -1,17 +1,38 @@
 'use strict'
 
-if (!util.promisify) { throw new Error('Requires node 8.x') }
-
-// core
-const util = require('util')
+// self
+const DatLru = require('./lib/dat-lru')
 
 // npm
-const Dat = util.promisify(require('dat-node'))
 const parse = require('parse-dat-url')
 const Koa = require('koa')
 const route = require('koa-route')
 const cors = require('kcors')
 
+let m = 0
+
+const lru = new DatLru('/tmp/false95959') // , { mkdirError: false }
+
+lru.isReady()
+  .then((a) => console.log('READY1', a))
+  .then(() => lru.get('abc1293'))
+  .then((value) => {
+    ++m
+    console.log('GET', m, value)
+    return lru.get('abc1235a')
+  })
+  .then((value) => {
+    ++m
+    console.log('GET', m, value)
+    return lru.get('abc1293')
+  })
+  .then((value) => {
+    ++m
+    console.log('GET', m, value)
+  })
+  .catch(console.error)
+
+/*
 const DAT_HASH_REGEX = /^[a-z0-9]{64}$/i
 
 const app = new Koa()
@@ -19,15 +40,16 @@ const app = new Koa()
 const vla = (ctx, datkey) => {
   const obj = parse(datkey)
   const key = obj.hostname
+  if (!DAT_HASH_REGEX.test(key)) { return Promise.reject(new Error('datkey not found')) }
 
-  if (!DAT_HASH_REGEX.test(key)) {
-    return Promise.reject(new Error('datkey not found'))
-  }
-
+*/
+/*
   return Dat('/tmp/temp-true999', { key, temp: false, sparse: true })
     .then((dat) => new Promise((resolve, reject) => {
+      dat.joinNetwork().once('connection', () => resolve(dat))
+      setTimeout(() => reject(new Error('Timeout')), 2000)
       // or use callback and check for
-      /*
+      ***
       dat.joinNetwork(function (err) {
         if (err) throw err
 
@@ -38,10 +60,10 @@ const vla = (ctx, datkey) => {
           process.exit(1)
         }
       })
-      */
-      dat.joinNetwork().once('connection', () => resolve(dat))
-      setTimeout(() => reject(new Error('Timeout')), 2000)
+      ***
     }))
+*/
+/*
 }
 
 const top = (ctx, datkey) => {
@@ -68,3 +90,4 @@ app.use(route.get('/profile/:datkey', datKeyM.bind(null, 'profile.json')))
 app.use(route.get('/top/:datkey', top))
 
 app.listen(3030)
+*/
