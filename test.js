e646bf23dcc33fc59b71e06e264fdad92e1aae05
Robin Millette <robin@millette.info>
2017-06-16 14:36:14 -0400

    Throw test.

--- test.js
+++ test.js
@@ -3,16 +3,12 @@ import test from 'ava'
 import { DatLru } from './'
 
 test('acc', async t => {
-  const lru = new DatLru('/tmp/false95959') // , { mkdirError: false }
+  const lru = new DatLru('/tmp/false95959')
   const store = await lru.isReady()
   t.truthy(store)
+})
 
-/*
-  const p = new Promise((resolve, reject) => {
-    resolve('ok')
-  })
-
-  let store = await p
-  t.is(store, 'ok')
-*/
+test('bad', async t => {
+  const lru = new DatLru('/tmp/false95959', { mkdirStrict: true })
+  await t.throws(lru.isReady())
 })
