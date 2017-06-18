68a969cb05e380a0237607ce191a46b8641c10ee
Robin Millette <robin@millette.info>
2017-06-18 15:21:59 -0400

    Use clivage and pass --fs to cli to force dat-lru-fs instead of dat-lru.

--- cli.js
+++ cli.js
@@ -5,35 +5,40 @@ const util = require('util')
 if (!util.promisify) { throw new Error('Requires node 8.x') }
 const pathResolve = require('path').resolve
 
-// self
-// const { DatLru, routes } = require('./')
-const DatLru = require('./lib/dat-lru')
-const routes = require('./lib/routes')
-
 // npm
 const Koa = require('koa')
 const route = require('koa-route')
 const cors = require('kcors')
 const conditional = require('koa-conditional-get')
 const etag = require('koa-etag')
+const joi = require('joi')
+const clivage = require('clivage')
+
+const schema = joi.object({
+  port: joi.number().integer().default(3030).optional(),
+  fs: joi.boolean().truthy('').optional()
+})
+
+const cli = clivage(schema)
+
+// self
+const routes = require('./lib/routes')
+const LRU = require(cli.flags.fs ? './lib/dat-lru-fs' : './lib/dat-lru')
+
+const lru = new LRU(pathResolve(__dirname, 'dat-tests'), { max: 15, mkdirError: false })
 
 const app = new Koa()
-
-const lru = new DatLru(pathResolve(__dirname, 'dat-tests'), { max: 15, mkdirError: false })
 app.context.datLru = lru
-
+app.context.cli = cli
 app.use(conditional())
 app.use(etag())
 app.use(cors({ allowMethods: 'GET' }))
 app.use(route.get('/resolve/:datkey', routes.datKeyM.bind(null, 'dat.json')))
 app.use(route.get('/profile/:datkey', routes.datKeyM.bind(null, 'profile.json')))
 app.use(route.get('/manifest/:datkey', routes.datKeyM.bind(null, 'manifest.json')))
-if (DatLru.cleanDat) {
-  app.use(route.get('/top/:datkey', routes.top))
-}
+
+if (cli.flags.fs) { app.use(route.get('/top/:datkey', routes.top)) }
 app.use(route.get('/version/:datkey', routes.version))
 app.use(route.get('/peers/:datkey', routes.peers))
 app.use(route.get('/', routes.home))
-app.listen(3030)
-
-console.log('app.env:', app.env)
+app.listen(cli.flags.port)
--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-// self
-const DatLru = require('./lib/dat-lru-fs')
-const routes = require('./lib/routes')
-
-module.exports = { DatLru, routes }
